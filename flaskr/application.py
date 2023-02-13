07a2d60f5cf9f01b6e688634466e1cd834275cba
etlu03 <etlu03@gmail.com>
2023-02-13 00:27:08 -0500

    Outlined multithreaded processes

--- flaskr/application.py
+++ flaskr/application.py
@@ -1,13 +1,35 @@
 from flask import Flask, request, render_template
+import asyncio
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
   if request.method == "POST":
-    print(request.form.get("url"))
+    url = request.form.get("url")
+    asyncio.run(retrieve(url))
     
   return render_template("./audit/home.html")
+
+async def worker(url, queue):
+  while True:
+    sleep_for = await queue.get()
+    await asyncio.sleep(sleep_for)
+    queue.task_done()
+    
+async def retrieve(url):
+  queue = asyncio.Queue()
+  tasks = []
+
+  task = asyncio.create_task(worker(url, queue))
+  tasks.append(task)
+
+  await queue.join()
+
+  for task in tasks:
+    task.cancel()
+  
+  await asyncio.gather(*tasks, return_exceptions=True)
 
 if __name__ == "__main__":
   app.run()
