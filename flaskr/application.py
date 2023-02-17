4dc381626e3c2546b61a3b66908217cbae80f739
etlu03 <etlu03@gmail.com>
2023-02-17 00:10:07 -0500

    Created a working audit search bar

--- flaskr/application.py
+++ flaskr/application.py
@@ -1,40 +1,63 @@
 from flask import Flask, request, render_template
+from bs4 import BeautifulSoup as bs
+
 import asyncio
+import requests
+import os
 
 app = Flask(__name__)
+seperator = " -- "
+
+destination = None
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-  '''
   if request.method == "POST":
     item = request.form.get("url")
     asyncio.run(retrieve(item))
-  '''
+    if destination is not None:
+      return render_template("./policies/" + destination)
+      
+  return render_template("./default/home.html")
 
-  return render_template("./default/waiting.html")
+def walk(url):
+  r = requests.get(url)
+  soup = bs(r.content, features="html.parser")
+  title = " ".join(soup.title.get_text().split())
+  
+  global destination
+  for filename in os.listdir("./templates/policies"):
+    if filename.endswith(".html"):
+      source = filename.split(seperator)[0]
+      if source == title:
+        destination = filename
+        
+  return destination
 
 async def producer(item, queue):
-  print("Producer: Running")
+  # print("Producer: Running")
   await queue.put(item)
   
   await queue.put(None)
-  print("Producer: Done")
+  # print("Producer: Done")
 
 async def consumer(queue):
-  print("Consumer: Running")
+  # print("Consumer: Running")
   while True:
     try:
       item = queue.get_nowait()
     except asyncio.QueueEmpty:
       await asyncio.sleep(0.5)
       continue
-    
+
     if item is None:
       break
+
+    walk(item)
+
+    print(f'> got item: {destination}')
   
-    print(f'> got item: {item}')
-  
-  print("Consumer: Done")
+  # print("Consumer: Done")
 
 async def retrieve(item):
   queue = asyncio.Queue()
